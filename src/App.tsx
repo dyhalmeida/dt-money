2bff167373d159511aae02e68686607a5fc2acb8
Diego Almeida <diego.almeida@memed.com.br>
2021-12-24 12:15:47 -0300

    Adicionado modal com react-modal
    Pontos importantes:
    
    1. O botão que ativa o modal se encontra no header, porém o modal em sí
    não faz sentido ser do contexto do header, por tanto foi colocado o modal
    no component raiz da aplicação e foi utilizado as props do componente header
    para aceitar a função do componente pai e executar a ação necessária.
    Dessa forma o component filho Header consegue executar uma função do component
    pai passada por props.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
+import { useState } from 'react';
+
 import { Dashboard } from './components/dashboard';
 import { Header } from './components/header';
+import ReactModal from 'react-modal';
+
 import { GlobalStyle } from './styles/global';
 
+ReactModal.setAppElement('#root');
+
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header 
+        onOpenNewTransactionModal={handleOpenNewTransactionModal} 
+      />
       <Dashboard />
+      <ReactModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        
+      </ReactModal>
       <GlobalStyle />
     </>
   );

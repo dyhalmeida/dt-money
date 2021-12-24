64448d1f3d7ef747af8f2e9fa2681208834868e6
Diego Almeida <diego.almeida@memed.com.br>
2021-12-24 14:10:58 -0300

    Adicionado estilo do form e botão de fechar modal

--- src/components/newTransactioModal/index.tsx
+++ src/components/newTransactioModal/index.tsx
@@ -1,6 +1,8 @@
 import ReactModal from 'react-modal';
 import { Container } from './style';
 
+import closeSVG from '../../assets/close.svg';
+
 interface NewTransactioModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -14,7 +16,23 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactioMod
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-    
+
+      <button 
+        type="button" 
+        onClick={onRequestClose}
+        className='react-modal-close'
+      >
+        <img src={closeSVG} alt="Fechar modal" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input type="text" placeholder='título' />
+        <input type="number" placeholder='valor' />
+        <input type="text" placeholder='categoria' />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </ReactModal>
   );
 }

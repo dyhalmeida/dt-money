cde1fdb46bdc94015a6b340b304569f29d674747
Diego Almeida <diego.almeida@memed.com.br>
2021-12-27 09:58:32 -0300

    Adicionado seed no miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Luz',
+          type: 'withdraw',
+          category: 'Energia',
+          amount: 100.87,
+          createdAt: new Date('2021-12-01 16:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Cata',
+          type: 'deposit',
+          category: 'Salário',
+          amount: 4900,
+          createdAt: new Date('2021-12-11 13:00:00'),
+        }
+      ]
+    })
+  },
+
   routes(){
     this.namespace = 'api';
     this.get('/transactions', () => {

ced4b03d8366b10b21e5edbe5cf3c299f633b326
Diego Almeida <diego.almeida@memed.com.br>
2021-12-28 08:46:23 -0300

    Adicionado context p/ transactions
    O ponto importante desse commit é a necessidade de compartilhar
    um estado da aplicação com demais componentes. Com isso o context
    cria um provider que engloba os componentes e assim todos os filhos
    do provider tem acesso a seus dados.
    
    Sempre que um estado do context for alterado, o componente que utiliza
    desse dado com o useContext sofrerá renderização

---
+++ src/context/TransactionsContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+export interface ITransaction {
+  id: number,
+  title: string,
+  type: string,
+  category: string,
+  amount: number,
+  createdAt: string,
+}
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+export const TransactionsContext = createContext<Array<ITransaction>>([]);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+
+  const [transactions, setTransactions] = useState<Array<ITransaction>>([]);
+
+  useEffect(() => {
+
+    api.get('transactions')
+      .then(response => setTransactions(response.data.transactions));
+
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={transactions}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
